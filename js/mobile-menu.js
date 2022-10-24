d4706f14ee90254bd1dda61b11888d4af7e66b91
YuliaPysarenko <y.pisarenko@ukr.net>
2022-10-24 15:49:49 +0300

    work

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,7 +5,7 @@
 
   const toggleMenu = () => {
     const isMenuOpen =
-    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
@@ -18,8 +18,7 @@
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
+  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
